24d0a25a91ab8da48e4bb01e1f7acb4417bd4291
Leilani Leach and Tanner Damron <tanner@>
2018-10-25 14:34:05 -0700

    add new arrays and for & while loop to return output, cleaned up indentation and styling

---
+++ README.md
@@ -0,0 +1,35 @@
+# Roman Numeral Translator
+## by Tanner Damron & Leilani Leach
+
+## Description
+A webpage that takes the user inputted number and runs it through a loop to check for each iteration of the index and returns the correct values and pushes it to the page.
+
+### Specifications
+Behavior: Takes inputted number and adds value of symbols
+Input: "2"
+Output: "II"
+
+Behavior: Convert numbers up to 3 numerals in a row
+Input: "3"
+Output: "III"
+
+Behavior: Subtract if more than 3 numerals in a row
+Input: "4"
+Output: "IV"
+
+### Setup Instructions
+* Clone repository
+* Add it to your desktop using "git clone""(repository link).""
+* Open in Atom to see all code used
+
+### Known Bugs
+Return text will append to last return text
+
+### Languages Used
+* HTML
+* CSS
+* Bootstrap
+* javascript
+* jQuery
+
+Copyright (c) 2018 Tanner Damron & Leilani Leach.
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,21 @@
-// var originalNumber="";
-
+var romanOutput = "";
 var numberArray = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-var numeralArray = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "IX", "V", "IV", "I"];
-// var inputArray = [];
-function romanNumerals(inputArray) {
-  for (var i = 1; i <= inputArray.length; i++) {
-  alert(numberArray + "!");
+var numeralArray = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+function romanNumerals(originalNumber) {
+  for (var i = 0; i < numberArray.length; i++) {
+    while (numberArray[i] <= originalNumber) {
+      romanOutput += numeralArray[i];
+      originalNumber -= numberArray[i];
+    }
   }
+  return romanOutput;
 }
-
-
-
 // front-end
 $(document).ready(function(){
-  $("form").submit(function(event){
+  $("form#roman").submit(function(event){
     event.preventDefault();
-    var originalNumber = $("input#userInput").val();
-    var inputArray = (originalNumber.split(""));
-    romanNumerals(inputArray);
+    var originalNumber = parseInt($("input#userInput").val());
+    romanNumerals(originalNumber);
+    $("#answer").text(romanOutput);
   });
 });
