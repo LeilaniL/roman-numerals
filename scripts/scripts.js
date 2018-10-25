3f17018758b6cd732b1e3ce8854848b6119f311e
Leilani Leach and Tanner Damron <leilani+tanner@>
2018-10-25 15:05:03 -0700

    fix variable scope, add clear variable inside function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,8 @@
-var romanOutput = "";
+var romanOutput;
 var numberArray = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 var numeralArray = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 function romanNumerals(originalNumber) {
+   romanOutput = "";
   for (var i = 0; i < numberArray.length; i++) {
     while (numberArray[i] <= originalNumber) {
       romanOutput += numeralArray[i];
